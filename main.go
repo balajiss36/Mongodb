ddf30709837b56e3cb3ff14242bdbc74a86ef6ec
Balaji Shettigar <balajishettigar36@gmail.com>
2022-05-01 02:51:20 +0530

    next commit

--- main.go
+++ main.go
@@ -15,11 +15,11 @@ func main() { // Entry point to the project
 	r.GET("/user/:id", uc.GetUser)
 	r.POST("/user", uc.CreateUser)
 	r.DELETE("/user", uc.DeleteUser)
-	http.ListenAndServe("localhost:8080", r) // Listen to port and put it to r.
+	http.ListenAndServe("localhost:9000", r) // Listen to port and put it to r.
 }
 
 func getSession() *mgo.Session {
-	s, err := mgo.Dial("mongodb://localhost:27107") // This is to connect with mongodb on port 27107
+	s, err := mgo.Dial("mongodb://localhost:27017") // This is to connect with mongodb on port 27107
 	//which is the default one and returns the session in the NewUserController function of controller
 	if err != nil {
 		panic(err)
